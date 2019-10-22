ae32789ec66f70387629517b026978c47cbfb204
Marcus Mouyat <marcus@mouyat.com>
2019-10-22 16:04:18 +1100

    Added files to repository

---
+++ Assets/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "ScriptableObjects/Items", order = 1)]
+public class Item : ScriptableObject
+{
+    public string description;
+    public Texture2D icon;
+    public GameObject mesh;
+
+    public int maxStack;
+    public int price;
+}
---
+++ Assets/ItemData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemData
+{
+    static string[] items = new string[]
+    {
+        ""
+    };
+
+    public static Item GetItemByID(int id)
+    {
+        return Resources.Load<Item>("Items/ItemObjects/" + items[id]);
+    }
+
+    public static Item GetItemByName(string name)
+    {
+        return Resources.Load<Item>("Items/ItemObjects/" + name);
+    }
+
+    public static Item[] GetAllItems()
+    {
+        //return Resources.LoadAll<Item>("Items/ItemObjects/");
+        return Resources.LoadAll<Item>("");
+    }
+}
